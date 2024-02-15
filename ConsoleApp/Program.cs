42ac929b55bdcd77e33ef49531bbc983e1473f99
Narendra <narendrakotika3576@gmail.com>
2024-02-15 12:27:28 +0530

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp;
+
+Console.WriteLine("Welcome to Authentication App");
+SignIn obj = new SignIn();
+Console.WriteLine(obj.Authenticate("admin", "admin@123"));
---
+++ ConsoleApp/SignIn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class SignIn
+    {
+        public string Authenticate(string userName, string password)
+        {
+            string msg;
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+            {
+                msg = "user name and password required";
+            }
+            else
+            {
+                if ((userName == "admin") && (password == "admin@123"))
+                {
+                    msg = "Authentication Pass";
+                }
+                else
+                {
+                    msg = "Authentication Fail";
+                }
+            }
+            return msg;
+        }
+    }
+
+}
+
